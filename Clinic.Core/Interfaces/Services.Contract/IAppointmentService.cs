03c87369c66343ae8a708a100a7da34762662fd8
IbrahimKaram <ahmedrmady0@gmail.com>
2024-07-24 18:32:01 +0300

    add the IAppointmentService interface

---
+++ Clinic.Core/Interfaces/Services.Contract/IAppointmentService.cs
@@ -0,0 +1,21 @@
+﻿using Clinc.ReponseModel;
+using Clinic.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Core.Interfaces.Services.Contract
+{
+    public interface IAppointmentService
+    {
+        Task<ResponseModel> CreateNewAppointment(Appointment appointment);
+
+        Task<IReadOnlyList<Appointment>> GetAppointments();
+        Task<IReadOnlyList<Appointment>> GetAppointmentsByDocId(int DocId);
+        Task<IReadOnlyList<Appointment>> GetAppointmentsByDocIdDuringPeriod(int DocId,DateTime From,DateTime To);
+
+
+    }
+}
---
+++ Clinic.Core/ReponseModel/ResponseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Clinc.ReponseModel
+{
+    public class ResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int TotalCount { get; set; }
+
+        public object Data { get; set; }
+        public ResponseModel()
+        {
+            this.IsSuccess = false;
+            Message = "حدث خطاء!";
+        }
+        public ResponseModel(string message, bool isSuccess =false,object Data = null)
+        {
+            this.IsSuccess = isSuccess;
+            this.Data = Data;
+            if (message is null)
+            {
+                this.Message = isSuccess ? "Successfully Done" : "Un Successfully";
+            }
+            else { this.Message = message; }
+        }
+    }
+}
